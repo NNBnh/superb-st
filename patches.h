516ed689bfaa6e20434dbda8edda5fee514bc245
NNB <nnbnh@protonmail.com>
2021-05-13 16:01:11 +0700

    deleted:    .gitmodules 	new file:   config.h 	deleted:    flexipatch 	new file:   patches.h

---
+++ patches.h
@@ -0,0 +1,26 @@
+#define ALPHA_PATCH              1
+#define ANYSIZE_PATCH            1
+#define ANYSIZE_NOBAR_PATCH      1
+#define VERTCENTER_PATCH         1
+#define RELATIVEBORDER_PATCH     1
+#define THEMED_CURSOR_PATCH      1
+#define HIDECURSOR_PATCH         1
+#define SPOILER_PATCH            1
+
+#define WIDE_GLYPHS_PATCH        1
+#define LIGATURES_PATCH          1
+#define BOLD_IS_NOT_BRIGHT_PATCH 1
+#define SYNC_PATCH               1
+
+#define OSC_10_11_12_2_PATCH     1
+#define BLINKING_CURSOR_PATCH    1
+#define UNDERCURL_PATCH          1
+
+#define DELKEY_PATCH             1
+#define FIXKEYBOARDINPUT_PATCH   1
+#define UNIVERSCROLL_PATCH       1
+
+#define SIXEL_PATCH              1
+#define W3M_PATCH                1
+#define WORKINGDIR_PATCH         1
+#define ST_EMBEDDER_PATCH        1
