76a9e49cba62975703e5892a0f180bd927841baf
NNB <43980777+NNBnh@users.noreply.github.com>
2021-05-26 13:31:37 +0700

    Update patches.h

--- patches.h
+++ patches.h
@@ -1,15 +1,20 @@
-#define ANYSIZE_PATCH            1
-#define RELATIVEBORDER_PATCH     1
-#define THEMED_CURSOR_PATCH      1
-#define ALPHA_PATCH              1
+#define ANYSIZE_PATCH                    1
+#define RELATIVEBORDER_PATCH             1
+#define THEMED_CURSOR_PATCH              1
+#define ALPHA_PATCH                      1
 
-#define WIDE_GLYPHS_PATCH        1
-#define LIGATURES_PATCH          1
+#define WIDE_GLYPHS_PATCH                1
+#define LIGATURES_PATCH                  1
+#define BOXDRAW_PATCH                    1
 
-#define BOLD_IS_NOT_BRIGHT_PATCH 1
-#define SYNC_PATCH               1
-#define W3M_PATCH                1
+#define BOLD_IS_NOT_BRIGHT_PATCH         1
+#define SYNC_PATCH                       1
+#define W3M_PATCH                        1
 
-#define OSC_10_11_12_2_PATCH     1
-#define BLINKING_CURSOR_PATCH    1
-#define UNDERCURL_PATCH          1
+#define OSC_10_11_12_2_PATCH             1
+#define BLINKING_CURSOR_PATCH            1
+#define UNDERCURL_PATCH                  1
+
+#define SCROLLBACK_MOUSE_ALTSCREEN_PATCH 1
+#define XRESOURCES_PATCH                 1
+#define XRESOURCES_RELOAD_PATCH          1
