c087362e97eb94225bf3b9381a2d8dd9b890808c
NNB <43980777+NNBnh@users.noreply.github.com>
2021-06-16 17:15:19 +0700

    Update patches.h

--- patches.h
+++ patches.h
@@ -9,6 +9,7 @@
 
 #define BOLD_IS_NOT_BRIGHT_PATCH         1
 #define SYNC_PATCH                       1
+#define SIXEL_PATCH                      1
 #define W3M_PATCH                        1
 
 #define OSC_10_11_12_2_PATCH             1
